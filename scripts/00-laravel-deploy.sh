cda66aa1407f3aedaed09b40ffadc4cbafa1ee0b
Syed Danish Anwar <syeddanishanwar@outlook.com>
2026-07-14 05:35:22 +0500

    Start web server in deployment script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,3 +5,10 @@ php artisan route:cache
 
 echo "Executing cloud database migrations..."
 php artisan migrate --force
+
+echo "Starting the web server processes..."
+# Start PHP-FPM in the background
+php-fpm -D
+
+# Start Nginx in the foreground to keep the container running
+nginx -g "daemon off;"
